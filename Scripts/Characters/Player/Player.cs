a7efedd0254281060cae585b487742621bb0806f
Caleb Foster <calebalakaifoster@gmail.com>
2026-08-11 20:39:08 -0700

    Player input

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,4 +3,12 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        GD.Print("Player Physics");
+    }
+    public override void _Input(InputEvent @event)
+    {
+        GD.Print("Player Input!");
+    }
 }
